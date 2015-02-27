a5eb5c9464d98b892fe9a768431a8258f75c1fa3
jwvg0425 <jwvg0425@naver.com>
2015-02-27 17:44:54 +0900

    gameManager

---
+++ src/HotlineMiagaemi/Classes/GameManager.cpp
@@ -0,0 +1,37 @@
+﻿#include "GameManager.h"
+
+GameManager* GameManager::m_Instance = nullptr;
+
+GameManager* GameManager::getInstance()
+{
+	if (m_Instance == nullptr)
+	{
+		m_Instance = new GameManager;
+	}
+
+	return m_Instance;
+}
+
+void GameManager::releaseInstance()
+{
+	if (m_Instance != nullptr)
+	{
+		delete m_Instance;
+		m_Instance = nullptr;
+	}
+}
+
+GameManager::GameManager()
+{
+
+}
+
+GameManager::~GameManager()
+{
+
+}
+
+Ant* GameManager::getAnt()
+{
+	return m_NowAnt;
+}
---
+++ src/HotlineMiagaemi/Classes/GameManager.h
@@ -0,0 +1,24 @@
+﻿#include "cocos2d.h"
+
+#ifndef _GAME_MANAGER_H_
+#define _GAME_MANAGER_H_
+
+class Ant;
+
+class GameManager
+{
+public:
+	static GameManager* getInstance();
+	void releaseInstance();
+	Ant* getAnt();
+
+private:
+	GameManager();
+	~GameManager();
+	Ant* m_NowAnt;
+
+	static GameManager* m_Instance;
+};
+
+
+#endif //_GAME_MANAGER_H_
