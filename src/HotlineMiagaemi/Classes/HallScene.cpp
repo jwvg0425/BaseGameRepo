7ea0091a3caead4a7296a3703995256e968153c9
jwvg0425 <jwvg0425@naver.com>
2015-02-28 06:44:38 +0900

    z-order 왜 날라갔냐

--- src/HotlineMiagaemi/Classes/HallScene.cpp
+++ src/HotlineMiagaemi/Classes/HallScene.cpp
@@ -29,7 +29,7 @@ Scene* HallScene::createScene()
 
 bool HallScene::init()
 {
-    if (!LayerColor::initWithColor(Color4B(255, 255, 255, 255)))
+    if (!LayerColor::initWithColor(Color4B(255, 255, 255, 0)))
     {
         return false;
     }
@@ -62,7 +62,7 @@ bool HallScene::init()
     addChild(m_ActMenu);
 
 	m_Sprite = HallSprite::create();
-	addChild(m_Sprite);
+	addChild(m_Sprite,-1);
     return true;
 }
 
